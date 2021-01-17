cb346fb82c81c503a5fd0dd000c2373b15a8922b
Tristan <mytic12@hotmail.fr>
2021-01-17 20:27:03 +0100

    :wrench: update url back

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? process.env.PROD_BACK_URL || "calm-crag-04725.herokuapp.com/api"
+    ? "https://calm-crag-04725.herokuapp.com/api"
     : "http://localhost:2020/api";
 export default API_URL;
